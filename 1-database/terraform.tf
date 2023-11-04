ea434cacf128acffe4b9a7615a25756c1a39add7
adriangg <adrian@dtssolutions.com.mx>
2023-11-04 17:22:38 +0000

    modify config the backed

--- 1-database/terraform.tf
+++ 1-database/terraform.tf
@@ -1,10 +1,11 @@
 terraform {
-  # backend "s3" {
-  #   bucket = ""
-  #   key    = "database/terraform.tfstate"
-  #   region = "us-east-1"
-  #   dynamodb_table = "" 
-  # }
+    backend "s3" {
+    #Modificar este bucket
+    bucket = "teraform-backend-sandbox-107"
+    key    = "sandbox2/database/terraform.tfstate"
+    region = "us-east-1"
+    #dynamodb_table = ""
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
